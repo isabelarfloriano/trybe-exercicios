3f490cec50e6dd9aa4dd9f79526b2cbf31a23539
Isabela Rozani Floriano <irf161295@gmail.com>
2022-03-07 22:07:32 -0300

    Exercicios parte 1

---
+++ fundamentos/bloco-07-introducao-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,10 @@
+const {sum} = require('./sum');
+
+describe('The function sum', () => {
+  it ('adds 4 + 5 to equal 9', () => {
+    expect (sum (4,5)).toBe (9);
+  })
+  it ('adds 0 + 0 to equal 0', () => {
+    expect (sum (0,0)).toBe (0);
+  })
+})
