740ea6c223651ad39b856156deb5d12ece2869af
Isabela Rozani Floriano <irf161295@gmail.com>
2022-04-11 12:53:59 -0300

    Att

---
+++ front-end/testando-meu-computador/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NameInput from './components/NameInput';
+
+function App() {
+  return (
+    <div className="App">
+      <fieldset>
+        <NameInput />
+      </fieldset>
+    </div>
+  );
+}
+
+export default App;
